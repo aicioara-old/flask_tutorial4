ea8c2012ab36cbfcd6b3891bef778cf8f50177f2
Andrei Cioara <andrei@cioara.me>
2019-05-17 09:29:23 +0300

    Factored in blueprint

--- flaskr/main/application.py
+++ flaskr/main/application.py
@@ -11,9 +11,33 @@ db = SQLAlchemy()
 def create_app(config_name='dev'):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(config_by_name[config_name])
+
     db.init_app(app)
 
-    from .blueprint import bp
+    bp = Blueprint('api', __name__, url_prefix='/api')
+
+    api = Api(bp,
+        title='Flask Restplus Api Boiler-plate with JWT',
+        description='A boilerplate for Flask-RESTplus web service',
+        version='1.0',
+        security='Bearer Auth',
+        authorizations={
+            'Bearer Auth': {
+                'type': 'apiKey',
+                'in': 'header',
+                'name': 'Authorization'
+            },
+        },
+        license='MIT',
+        license_url='https://opensource.org/licenses/MIT',
+    )
+
+    from .views.user_views import api as user_ns
+    api.add_namespace(user_ns)
+
+    from .views.auth_views import api as auth_ns
+    api.add_namespace(auth_ns)
+
     app.register_blueprint(bp)
 
     return app
