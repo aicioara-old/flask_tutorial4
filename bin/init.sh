02b4bb1b0c68b1d73f30848ea22d8f3292f73318
Andrei Cioara <andrei@cioara.me>
2019-05-17 09:48:34 +0300

    Moved tests up two levels

--- bin/init.sh
+++ bin/init.sh
@@ -6,7 +6,8 @@ THIS_DIR=$(dirname "$0")
 cd ${THIS_DIR}
 cd ..
 
-
 source venv/bin/activate
 
-FLASK_APP=flaskr FLASK_ENV=development flask init-db
+FLASK_ENV=development python manage.py db init
+FLASK_ENV=development python manage.py db migrate
+FLASK_ENV=development python manage.py db upgrade
--- manage.py
+++ manage.py
@@ -24,7 +24,7 @@ def run():
 @manager.command
 def test():
     """Runs the unit tests."""
-    tests = unittest.TestLoader().discover('flaskr/test/backend', pattern='test*.py')
+    tests = unittest.TestLoader().discover('test/backend', pattern='test*.py')
     result = unittest.TextTestRunner(verbosity=2).run(tests)
     if result.wasSuccessful():
         return 0
