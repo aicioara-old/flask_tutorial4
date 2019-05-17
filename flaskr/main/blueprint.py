3ac3a57a87e6883d6f9d9402cd1ff9d3cd892353
Andrei Cioara <andrei@cioara.me>
2019-05-17 08:14:22 +0300

    Minor refactoring

--- flaskr/main/blueprint.py
+++ flaskr/main/blueprint.py
@@ -6,23 +6,21 @@ from .controller.auth_controller import api as auth_ns
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
-authorizations = {
-    'Bearer Auth': {
-        'type': 'apiKey',
-        'in': 'header',
-        'name': 'Authorization'
-    },
-}
-
 api = Api(bp,
     title='Flask Restplus Api Boiler-plate with JWT',
     description='A boilerplate for Flask-RESTplus web service',
     version='1.0',
     security='Bearer Auth',
-    authorizations=authorizations,
+    authorizations={
+        'Bearer Auth': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        },
+    },
     license='MIT',
     license_url='https://opensource.org/licenses/MIT',
 )
 
-api.add_namespace(user_ns, path='/user')
+api.add_namespace(user_ns)
 api.add_namespace(auth_ns)
--- flaskr/main/util/dto.py
+++ flaskr/main/util/dto.py
@@ -4,17 +4,17 @@ from flask_restplus import Namespace, fields
 class UserDto:
     api = Namespace('user', description='user related operations')
     user = api.model('user', {
-        'email': fields.String(required=True, description='user email address'),
-        'username': fields.String(required=True, description='user username'),
-        'password': fields.String(required=True, description='user password'),
-        'public_id': fields.String(description='user Identifier')
+        'email': fields.String(required=True, description='User email address'),
+        'username': fields.String(required=True, description='User username'),
+        'password': fields.String(required=True, description='User password'),
+        'public_id': fields.String(description='User ID')
     })
 
 
 
 class AuthDto:
     api = Namespace('auth', description='authentication related operations')
-    user_auth = api.model('auth_details', {
+    user_auth = api.model('auth', {
         'email': fields.String(required=True, description='The email address'),
         'password': fields.String(required=True, description='The user password '),
     })
